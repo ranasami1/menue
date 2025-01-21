ba317b25de9b4871e375c030cd6092e112beddf7
ranasami1 <ranasami237@gmail.com>
2025-01-21 11:18:57 +0200

    as prev

--- menue/src/Components/Header/Header.js
+++ menue/src/Components/Header/Header.js
@@ -1,14 +1,16 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar'; 
+import logo from '../assets/logo.png';
+import "./Header.css"
 
 import '../Header/Header.css';
 function Header() {
   return (
     <>
-      <Navbar className='Navbar' bg="dark" data-bs-theme="dark">
+      <Navbar className='Navbar' bg="black" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="/"><h1>ROLANDS <span style={{color:"#F8AD42"}}>CAFFEE</span></h1></Navbar.Brand>
+          <Navbar.Brand href="/"><img src={logo} style={{maxHeight:"90px"}}></img></Navbar.Brand>
           <Nav>
             <Nav.Link href="/">Menue</Nav.Link>
             <Nav.Link href="/aboutus">Contact Us</Nav.Link>
