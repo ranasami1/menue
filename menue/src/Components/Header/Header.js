22be9f9bd7a056115ac73fe568ee1b7998f4efda
ranasami1 <ranasami237@gmail.com>
2025-01-22 15:01:49 +0200

    add some translitions

--- menue/src/Components/Header/Header.js
+++ menue/src/Components/Header/Header.js
@@ -9,15 +9,17 @@ import { useContext } from 'react';
 import { AppContext } from '../../App';
 import { Button } from 'react-bootstrap';
 function Header() {
-  const { t } = useContext(AppContext);
+  const { t,lng } = useContext(AppContext);
   return (
       <Navbar bg="black" className='Navbar' data-bs-theme="warning" >
         <Container >
         <Navbar.Brand href="/"><img src={logo} style={{maxWidth:"180px"}}></img></Navbar.Brand>
         <Nav className="my-auto mb-0">
         <Nav.Link href="/" className="custom-link">{t("Categories")}</Nav.Link>
-        <Button variant="warning" onClick={()=>{i18next.changeLanguage("en");}}>en</Button>
-        
+        {lng === "en"?
+        <Button variant="warning" onClick={()=>{i18next.changeLanguage("ar")
+        }}>ar</Button>:<Button variant="warning" onClick={()=>{i18next.changeLanguage("en")
+        }}>en</Button>}
         </Nav>
         </Container>
       </Navbar>
