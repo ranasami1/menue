d1e42eb43e04e430dc9d6fb05bb43a279cf8d70e
ranasami1 <ranasami237@gmail.com>
2025-01-22 13:47:09 +0200

    done

--- menue/src/Components/Sec2/Sec2.js
+++ menue/src/Components/Sec2/Sec2.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext} from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
