6f5af8f94d85379a02c6027521691170e06a3e16
ranasami1 <ranasami237@gmail.com>
2025-01-22 13:52:36 +0200

    redeploy

--- menue/src/Components/Menue/Menue.js
+++ menue/src/Components/Menue/Menue.js
@@ -12,17 +12,17 @@ export function Menue(){
     const retrievedObject = JSON.parse(storedJsonString);
     return(
         <div className="menue">
-            <h1 className='title tracking-in-expand'>{lng == "ar" ?retrievedObject.ar_category_name:retrievedObject.en_category_name}</h1>
+            <h1 className='title tracking-in-expand'>{lng === "ar" ?retrievedObject.ar_category_name:retrievedObject.en_category_name}</h1>
             <div className="container-fluid d-flex gap-3 align-items-center justify-content-center flex-wrap p-5">
                 {retrievedObject?retrievedObject.items?.details?.map((item,key)=>{
                     return(
                         <Card key={key} style={{ margin:"10px 0"}}>
                             <Card.Img src={item.image} style={{ maxHeight: '230px' }} />
                             <Card.Body>
-                                <Card.Title>{lng=="en"?item.en_name:item.ar_name}</Card.Title>
+                                <Card.Title>{lng === "en"?item.en_name:item.ar_name}</Card.Title>
                                <div className='d-flex justify-content-between'>
-                                <p style={{backgroundColor:"red",padding:"5px",borderRadius:"5px",color:"#ffff",fontWeight:"bold",margin: '0 20px'}}>{lng == "ar" ? `${item.price} ${t("SAR")}` : `${t("SAR")} ${item.price}`}</p>
-                                <p style={{backgroundColor:"green",padding:"5px",borderRadius:"5px",color:"#ffff",fontWeight:"bold",margin: '0 20px'}}>{lng == "ar" ? `${item.calories} ${t("Calories")}` :`${t("Calories")} ${item.calories}`}
+                                <p style={{backgroundColor:"red",padding:"5px",borderRadius:"5px",color:"#ffff",fontWeight:"bold",margin: '0 20px'}}>{lng === "ar" ? `${item.price} ${t("SAR")}` : `${t("SAR")} ${item.price}`}</p>
+                                <p style={{backgroundColor:"green",padding:"5px",borderRadius:"5px",color:"#ffff",fontWeight:"bold",margin: '0 20px'}}>{lng === "ar" ? `${item.calories} ${t("Calories")}` :`${t("Calories")} ${item.calories}`}
                                 </p>
                                </div>
                             </Card.Body>
