c769679f9234a7bfad91ae00da100166c7a65d68
ranasami1 <ranasami237@gmail.com>
2025-01-24 11:37:40 +0200

    social links all target _blank

--- menue/src/Components/Footer/Footer.js
+++ menue/src/Components/Footer/Footer.js
@@ -13,9 +13,9 @@ export default function Footer() {
         <div className='container-fluid' id="contact">
         <h4>{t("Contact Us")}</h4>
         <div className='d-flex gap-4 align-items-center justify-content-center flex-wrap'>
-        <a href='https://x.com/RolllandsCoffe?fbclid=IwAR1SJrG0QjNi4SzkBblHTZv0cJs-ifBHA1g95og47IcwjE9EDWkY7DOC-24'><FontAwesomeIcon icon={faXTwitter} /></a>
-        <a href='https://www.facebook.com/RolllandsCoffe'><FontAwesomeIcon icon={faFacebook} /></a>
-        <a href='https://www.youtube.com/channel/UCRST_Si9jDyE-5WQ7_6HnkA'><FontAwesomeIcon icon={faYoutube} /></a>
+        <a href='https://x.com/RolllandsCoffe?fbclid=IwAR1SJrG0QjNi4SzkBblHTZv0cJs-ifBHA1g95og47IcwjE9EDWkY7DOC-24' target='_blank'><FontAwesomeIcon icon={faXTwitter} /></a>
+        <a href='https://www.facebook.com/RolllandsCoffe' target='_blank'><FontAwesomeIcon icon={faFacebook} /></a>
+        <a href='https://www.youtube.com/channel/UCRST_Si9jDyE-5WQ7_6HnkA' target='_blank'><FontAwesomeIcon icon={faYoutube} /></a>
         <a href='tel:+966590968866'>+966 59 096 8866</a>
         </div>
         
