065df9345bfc57112cc02c8aa7db4f714d8c2fac
ranasami1 <ranasami237@gmail.com>
2025-01-24 11:45:21 +0200

    fix phone number direction

--- menue/src/Components/Footer/Footer.js
+++ menue/src/Components/Footer/Footer.js
@@ -16,7 +16,7 @@ export default function Footer() {
         <a href='https://x.com/RolllandsCoffe?fbclid=IwAR1SJrG0QjNi4SzkBblHTZv0cJs-ifBHA1g95og47IcwjE9EDWkY7DOC-24' target='_blank'><FontAwesomeIcon icon={faXTwitter} /></a>
         <a href='https://www.facebook.com/RolllandsCoffe' target='_blank'><FontAwesomeIcon icon={faFacebook} /></a>
         <a href='https://www.youtube.com/channel/UCRST_Si9jDyE-5WQ7_6HnkA' target='_blank'><FontAwesomeIcon icon={faYoutube} /></a>
-        <a href='tel:+966590968866'>+966 59 096 8866</a>
+        <a id="phone" href='tel:+966590968866'>+966 59 096 8866</a>
         </div>
         
         </div>
