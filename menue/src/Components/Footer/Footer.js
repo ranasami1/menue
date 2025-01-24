b3f942128959c08246542603b2b930552f6598c2
ranasami1 <ranasami237@gmail.com>
2025-01-24 11:41:13 +0200

    body background color

--- menue/src/Components/Footer/Footer.js
+++ menue/src/Components/Footer/Footer.js
@@ -7,7 +7,7 @@ import { useTranslation } from 'react-i18next';
 export default function Footer() {
   let { t } = useTranslation();
   return (
-    <footer>
+    <footer className='mt-2'>
       <p>{t ("Prices include VAT")}</p>
       <p>{t("Adults need 2000 calories per day")}</p>
         <div className='container-fluid' id="contact">
